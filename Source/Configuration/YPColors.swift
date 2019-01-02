d034a0c0cb74d0a6d4f3a4a26b3cfad9d1fc9a8c
Julien PIPARD <julien.pipard@gmail.com>
2019-01-02 20:07:02 +0100

    styles

--- Source/Configuration/YPColors.swift
+++ Source/Configuration/YPColors.swift
@@ -24,26 +24,26 @@ public struct YPColors {
     public var multipleItemsSelectedCircleColor: UIColor?
     
     /// The background color of the bottom of photo and video screens.
-    public var photoVideoScreenBackground: UIColor = YPConfig.colors.color1
+    public var photoVideoScreenBackground: UIColor = UIColor.white
     
     // MARK: - Trimmer
     
     /// The color of the main border of the view
     public var trimmerMainColor: UIColor = UIColor.black
     /// The color of the handles on the side of the view
-    public var trimmerHandleColor: UIColor = YPConfig.colors.color1
+    public var trimmerHandleColor: UIColor = UIColor.white
     /// The color of the position indicator
-    public var positionLineColor: UIColor = YPConfig.colors.color1
+    public var positionLineColor: UIColor = UIColor.white
     
    // MARK: - Cover selector
     
     /// The color of the cover selector border
-    public var coverSelectorBorderColor: UIColor = YPConfig.colors.color1
+    public var coverSelectorBorderColor: UIColor = UIColor.white
     
     // MARK: - Progress bar
     
     /// The color for the progress bar when processing video or images. The all track color.
-    public var progressBarTrackColor: UIColor = YPConfig.colors.color1
+    public var progressBarTrackColor: UIColor = UIColor.white
     /// The color of completed track for the progress bar
     public var progressBarCompletedColor: UIColor?
     
